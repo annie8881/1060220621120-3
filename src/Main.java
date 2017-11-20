f6bc9cea0c58959db33927086f29cd6a75e31a26
ammie8881 <880801blue@gmail.com>
2017-11-20 14:54:21 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int i;
+        for(i=2;i<n;i++)
+        {
+            if(n%i==0)
+                break;
+        }
+        if(i==n)
+            System.out.println("YES");
+        else
+            System.out.println("No");
+    }
+}
